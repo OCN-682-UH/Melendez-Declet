8d8b4a050aae0aeef6114c3143e89d078cdeb6cd
carolinamelendezdeclet <carolina.melendez3@upr.edu>
2026-09-08 13:42:04 -1000

    Testing

--- Week_02/Scripts/introscript.R
+++ Week_02/Scripts/introscript.R
@@ -23,5 +23,5 @@ view (WeightData) # Shows you the full data, I also like to click on the data in
 #Step 1: Go to your terminal 
 # Step 2: Github Add: "C:\Users\mikae\Downloads\Melendez-Declet>git add ."
 # Step 3: Github commit "C:\Users\mikae\Downloads\Melendez-Declet>git commit -a -m "Include any comments"
-# Step 4: Github push: ""C:\Users\mikae\Downloads\Melendez-Declet>git add ."
+# Step 4: Github push: ""C:\Users\mikae\Downloads\Melendez-Declet>git push ."
 
---
+++ Week_03/intro_plotting.R
@@ -0,0 +1,13 @@
+###### Intro to Plotting Script #######
+# Carolina Melendez Declet
+# cvmd@hawaii.edu
+# 09/08/2026 
+# This script is to learn how to use ggplot and practice creating plots. 
+
+###### Personal Notes ######
+# Adding script to Git Hub 
+
+# Step 1: Go to your terminal 
+# Step 2: Github Add: "C:\Users\mikae\Downloads\Melendez-Declet>git add ."
+# Step 3: Github commit "C:\Users\mikae\Downloads\Melendez-Declet>git commit -a -m "Include any comments"
+# Step 4: Github push: ""C:\Users\mikae\Downloads\Melendez-Declet>git add ."
